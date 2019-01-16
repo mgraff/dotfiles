d1249d3d8e1a678d806cb7977a85cca7146c78a6
Maxime Graff <mgraff@denyall.com>
2019-01-16 15:52:23 +0100

    Add zprezto dotfiles

---
+++ README.md
@@ -0,0 +1,8 @@
+# Dotfiles
+
+## Installation
+
+To install the symlinks in the $HOME directory, just run:
+
+    ./install.sh
+
--- install.sh
+++ install.sh
@@ -2,8 +2,15 @@
 
 REPO_DIR=$(realpath $(dirname $0))
 
+# Install symlinks
 find "${REPO_DIR}" -name "*.symlink" -print | while read file
 do
-  dest="${HOME}/$(basename $(echo "${file}" | sed 's/.symlink$//'))"
-  ln -sfv "${file}" "${dest}"
+    dest="${HOME}/$(basename $(echo "${file}" | sed 's/.symlink$//'))"
+    ln -sfv "${file}" "${dest}"
+done
+
+# Install zprezto
+find "${HOME}"/.zprezto/runcoms/ -type f \! -name 'README.md' | while read file
+do
+    ln -sfv "${file}" "${HOME}/.$(basename "${file}")"
 done
