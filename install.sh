2d64b6e4fe606833f7acfff57979ecebb6062a48
Maxime Graff <max@rsky.fr>
2019-01-16 23:13:37 +0100

    Refactor tmux configuration and get rid of the old subrepo

--- install.sh
+++ install.sh
@@ -3,14 +3,14 @@
 REPO_DIR=$(realpath $(dirname $0))
 
 # Install symlinks
-find "${REPO_DIR}" -name "*.symlink" -print | while read file
+find "${REPO_DIR}" -not -iwholename '*.git*' -name "*.symlink" -print | while read file
 do
     dest="${HOME}/$(basename $(echo "${file}" | sed 's/.symlink$//'))"
     ln -sfv "${file}" "${dest}"
 done
 
 # Install zprezto
-find "${HOME}"/.zprezto/runcoms/ -type f \! -name 'README.md' | while read file
+find "${HOME}"/.zprezto/runcoms -type f \! -name 'README.md' | while read file
 do
     ln -sfv "${file}" "${HOME}/.$(basename "${file}")"
 done
