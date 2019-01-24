c5d2692ad204e0b83b13ef5ac0b9057f2aa68ab8
Maxime Graff <mgraff@denyall.com>
2019-01-24 18:09:12 +0100

    [tmux] add tmux-resurrect and tmux-continuum to save and restore environment

--- install.sh
+++ install.sh
@@ -7,19 +7,19 @@ echo "* Installing *.symlink files"
 find "${REPO_DIR}" -not -iwholename '*.git*' -name "*.symlink" -print | while read file
 do
     dest="${HOME}/$(basename $(echo "${file}" | sed 's/.symlink$//'))"
-    ln -sfv "${file}" "${dest}"
+    ln -sfnv "${file}" "${dest}"
 done
 
 # Install zprezto
 echo "* Installing zprezto dotfiles"
 find "${HOME}"/.zprezto/runcoms -type f \! -name 'README.md' | while read file
 do
-    ln -sfv "${file}" "${HOME}/.$(basename "${file}")"
+    ln -sfnv "${file}" "${HOME}/.$(basename "${file}")"
 done
 
 # Install kitty
 if type kitty; then
     echo "* Installing kitty dotfiles"
     mkdir -p ${HOME}/.config
-    ln -sfv ${REPO_DIR}/kitty ${HOME}/.config/
+    ln -sfnv ${REPO_DIR}/kitty ${HOME}/.config/
 fi
